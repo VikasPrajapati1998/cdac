5e5d5d38498ce2ee3dbb378ed55ad73bd03d34cf
Er. Vikas Prajapati <81795265+VikasPrajapati1998@users.noreply.github.com>
2023-04-28 21:51:40 +0530

    Update string_operations.py

--- code/src/string_operations/string_operations.py
+++ code/src/string_operations/string_operations.py
@@ -1,3 +1,6 @@
 def reduce_whitespace():
     with open("../../data/white_spaces.txt", 'r') as file:
         file.read()
+        
+__author__      = "Vikas Prajapati"
+__copyright__   = "Copyright 2023, CDAC-PGDBDA, Bangalore"
